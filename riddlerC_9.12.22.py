c7799d27aaaa3e126e9148b535c33b2016531ea6
Mamauu <79335054+Mamauu@users.noreply.github.com>
2022-12-14 15:45:58 +0100

    Update riddlerC_9.12.22.py

--- riddlerC_9.12.22.py
+++ riddlerC_9.12.22.py
@@ -8,13 +8,10 @@ def get_quality():
         "c": random.random(),
         "d": random.random()
             }
-    print(teams)
     return teams
 
 def play(name1,name2):  
-    q1 = teams[name1]
-    q2 = teams[name2]
-    win_percentage = q1/(q1+q2)
+    win_percentage = teams[name1]/(teams[name1]+teams[name2])
     if random.random() < win_percentage:
         return name1
     else:
@@ -28,18 +25,12 @@ for i in range(sample_size):
     
     #semifinal 1
     winner1 = play("a","b")
-    #print("semi1:", winner1)
-    
     #semifinal 1
     winner2 = play("c","d")
-    #print("semi2:", winner2)
-    
     #final
     winner3 = play(winner1,winner2)
     winner_q_list.append(teams[winner3]) #write winning quality in list
-    #print("final:", winner3, teams[winner3])
 
 avq = np.average(winner_q_list)
 print(avq)
-
 #answer = 0.655085829092286
